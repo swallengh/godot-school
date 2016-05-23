209f0d21beeccb4bd29ae40a737a0ce38c7201e2
David Vargas <davidvargas.tenerife@gmail.com>
2016-05-23 10:50:10 +0100

    N first commit

---
+++ shooter-game1/player/player.gd
@@ -0,0 +1,32 @@
+
+extends Area2D
+
+const SPEED = 200
+
+func _process(delta):
+	var dir=Vector2()
+	
+	if (Input.is_action_pressed("player_down")):
+		dir+=Vector2(0,1)
+	if (Input.is_action_pressed("player_up")):
+		dir+=Vector2(0,-1)
+	if (Input.is_action_pressed("player_left")):
+		dir+=Vector2(-1,0)
+	if (Input.is_action_pressed("player_right")):
+		dir+=Vector2(1,0)
+	
+	var pos = get_pos()
+	pos+=dir * delta * SPEED
+	var size = get_viewport_rect().size
+	
+	pos.x = clamp(pos.x, 0, size.x)
+	pos.y = clamp(pos.y, 0, size.y)
+	
+	set_pos(pos)
+
+
+func _ready():
+	# Called every time the node is added to the scene.
+	# Initialization her
+	set_process(true)
+
