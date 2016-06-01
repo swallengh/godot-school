ef985f087493bf8cabac7fa3bb0837915a40e9e3
David Vargas <davidvargas.tenerife@gmail.com>
2016-06-01 11:20:35 +0100

    U more IA to enemy

--- games/01-shooter/enemy/enemy.gd
+++ games/01-shooter/enemy/enemy.gd
@@ -2,14 +2,16 @@
 extends Area2D
 
 func _ready():
-	pass
+	set_process(true)
 
+func _process(delta):
+	pass
+	
 func _on_enemy_area_enter( area ):
 	if (area.get_name()=="player"):
 		area.exploit()
 	
 	if (area extends preload("res://player/shot.gd")):
-		area.kill_enemy()
+		area.queue_free()
 		get_tree().get_root().get_node("level1/sample").play("exploit_enemy")
-		get_tree().get_root().get_node("level1/player").enemy_killed()
 		queue_free()
