a1e31be3e751ec58f21067ece548e9e7a5a7c1b8
David Vargas <davidvargas.tenerife@gmail.com>
2016-06-06 11:20:18 +0100

    U

--- games/01-shooter/enemy/enemy.gd
+++ games/01-shooter/enemy/enemy.gd
@@ -1,25 +1,25 @@
 
 extends Area2D
 
-export var speed = Vector2(0,150)
-export var max_speed = Vector2(0,400)
+var speed = Vector2(0,150)
+export var max_speed = Vector2(50,400)
 export var min_speed = Vector2(0,10)
-var size = null
-var pos =null
-
-func init():
-	pos = get_pos()
-	pos.x = randi()%int(size.x)
-	pos.y=0
-	speed.x = randi()%200-100
-	speed.y = randi()%int(max_speed.y)+int(min_speed.y)
-	set_pos(pos)
+var size = Vector2(0,0)
+var pos = Vector2(0,0)
 
 func _ready():
 	size = get_viewport_rect().size
 	init()
-	pos.y = (randi()%int(speed.y))*-1
+	pos.y = (randi()%int(speed.y))*-2
 	set_process(true)
+
+func init():
+	pos.x = randi()%int(size.x)
+	pos.y=0
+	#speed.x = randi()%int(max_speed.x)-int(min_speed.x)
+	speed.x= randi()%100-50
+	speed.y = randi()%int(max_speed.y)+int(min_speed.y)
+	set_pos(pos)
 
 func _process(delta):
 	translate(Vector2( speed.x * delta , speed.y * delta))
@@ -33,7 +33,7 @@ func _on_enemy_area_enter( area ):
 		var level = get_tree().get_root().get_node("level")
 		level.get_node("sample").play("exploit_enemy")
 		level.inc_pts()
-		queue_free()
+		init()
 
 func _on_VisibilityNotifier2D_exit_screen():
 	init()
--- games/01-shooter/level/level1.gd
+++ games/01-shooter/level/level1.gd
@@ -1,10 +1,13 @@
 
 extends Node2D
 
-export var max_enemies = 10
-export var pts = 0
+export var max_enemies = 0
+export var max_time = 60.0
+var pts = 0
+var total_time = 0.0
 
 func _ready():
+	set_process(true)
 	show_pts()
 	for i in range(max_enemies):
     	build_enemy()
@@ -15,8 +18,15 @@ func build_enemy():
 	get_node("enemies").add_child( enemy_scene )
 
 func show_pts():
-	get_node("panel/pts").set_text( str(pts) )
+	get_node("panel/pts_text").set_text( str(pts) )
 	
 func inc_pts():
 	pts = pts + 10
 	show_pts()
+
+func _process(delta):
+	total_time = total_time + delta
+	if (total_time > max_time):
+		get_tree().quit()
+		
+
