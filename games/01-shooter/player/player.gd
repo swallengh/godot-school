1f7c7486fdde419e83f1618b43424701f6931667
David Vargas <davidvargas.tenerife@gmail.com>
2016-06-07 08:33:18 +0100

    U clear script

--- games/01-shooter/player/player.gd
+++ games/01-shooter/player/player.gd
@@ -4,32 +4,36 @@ extends Area2D
 const SPEED = 200
 var exploted = false
 var prev_shoot = false
-var killed = 0
+var total_time = 0
 
 func _ready():
 	set_process(true)
 
 func _process(delta):
 	if exploted:
+		total_time = total_time + delta
+		#The player is dead and we are waiting a few seconds before quit
+		if (total_time > 3):
+			get_tree().quit()
 		return
+
 	var dir=Vector2()
-	
-	if (Input.is_action_pressed("player_down")):
+	if (Input.is_action_pressed("player_down")): #Move player down
 		dir+=Vector2(0,1)
-	if (Input.is_action_pressed("player_up")):
+	if (Input.is_action_pressed("player_up")): #Move player up
 		dir+=Vector2(0,-1)
-	if (Input.is_action_pressed("player_left")):
+	if (Input.is_action_pressed("player_left")): #Move player left
 		dir+=Vector2(-1,0)
-	if (Input.is_action_pressed("player_right")):
+	if (Input.is_action_pressed("player_right")): #Move player right
 		dir+=Vector2(1,0)
 	
 	var pos = get_pos()
-	pos+=dir * delta * SPEED
-	var size = get_viewport_rect().size
+	pos+=dir * delta * SPEED #This is our new position
 	
-	pos.x = clamp(pos.x, 0, size.x)
+	var size = get_viewport_rect().size
+	pos.x = clamp(pos.x, 0, size.x) #The player can't exit screen
 	pos.y = clamp(pos.y, 0, size.y)
-	set_pos(pos)
+	set_pos(pos) #set our new position, move the player
 	
 	var shoot = Input.is_action_pressed("player_shot")
 	if (shoot and not prev_shoot):
@@ -41,13 +45,10 @@ func _process(delta):
 	prev_shoot = shoot
 
 func exploit():
-	if (exploted):
+	if (exploted): #The player can't be exploted now
 		return
-	get_node("sprite").hide()
-	get_node("explosion").set_emitting(true)
-	get_node("sample").play("explosion")
-	exploted=true
+	get_node("sprite").hide() #Hide the image
+	get_node("explosion").set_emitting(true) #Active explosion effect
+	get_node("sample").play("explosion") #Play the explosion sound
+	exploted=true #Now player is in exploted state, bye bye
 	
-func enemy_killed():
-	killed += 1
-	#get_tree().get_root().get_node("level1/info").
