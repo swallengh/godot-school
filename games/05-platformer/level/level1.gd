3eee351624664f945241485433234e19fd0a7139
David Vargas <davidvargas.tenerife@gmail.com>
2016-06-13 10:00:06 +0100

    F problems wth physics

--- games/05-platformer/level/level1.gd
+++ games/05-platformer/level/level1.gd
@@ -3,29 +3,23 @@ extends Node2D
 
 var player = null
 var ray = null
-var speed = Vector2(8000,9000)
+var speed = Vector2(8000,30000)
 var state="stand"
 var dir="left"
 var anim_name= state+"_"+dir
+var wait = true
 
 func _ready():
 	player = get_node("player")
 	ray = get_node("player/ray")
-	#ray.add_exception(player)
+	ray.add_exception(player)
 	get_node("player/anim").play(anim_name)
 	set_process(true)
 	
 func _process(delta):
-	if Input.is_key_pressed(81):
+	if Input.is_key_pressed(16777217) or Input.is_key_pressed(81):
 		get_tree().quit()
 		
-	var s = state+"_"+dir
-	if s!=anim_name:
-		if state=="jump":
-			state="move"
-		anim_name=state+"_"+dir
-		get_node("player/anim").play(anim_name)
-
 	if on_ground():
 		if Input.is_action_pressed("player_jump"):
 			player.set_axis_velocity(Vector2(0, -speed.y*delta))
@@ -40,8 +34,15 @@ func _process(delta):
 			dir="right"
 		else:
 			state="stand"
+		
+		var s = state+"_"+dir
+		if s!=anim_name:
+			anim_name=s
+			get_node("player/anim").play(anim_name)
+
 	else:
-		state="jump"
+		if player.get_linear_velocity().y >0:
+			state="fall"
 		
 	print("state => ",state,", dir =>",dir)
 			
