00779ce04c5572705f2fda64e53946e013349c9d
David Vargas <davidvargas.tenerife@gmail.com>
2016-10-22 20:17:44 +0100

    U hand

--- games/06-block-tower/hand/hand.gd
+++ games/06-block-tower/hand/hand.gd
@@ -1,6 +1,8 @@
 
 extends Node2D
 var SPEED = 200
+var last_drop_time = 0.0
+var max_drop_time = 2.0
 
 func _ready():
 	set_process(true)
@@ -12,3 +14,16 @@ func _process(delta):
 	if Input.is_action_pressed("ui_right"):
 		desp =  delta * SPEED
 	translate(Vector2(desp,0))
+
+	last_drop_time -= delta
+	var pos = get_pos()
+	
+	var drop = Input.is_action_pressed("ui_accept")
+	if (drop and last_drop_time<=0):
+		var square_res = preload("res://blocks/square.xml")
+		var square_scn = square_res.instance()
+		pos.x = pos.x + 48
+		pos.y = pos.y + 78
+		square_scn.set_pos( pos )
+		get_parent().add_child( square_scn )
+		last_drop_time = max_drop_time 
