94a600bd2b0b8944f45b93fe7c24b2a4bd6ee89a
David Vargas <davidvargas.tenerife@gmail.com>
2016-11-07 11:12:43 +0000

    U hand move automatically

--- games/06-block-tower/hand/hand.gd
+++ games/06-block-tower/hand/hand.gd
@@ -1,6 +1,7 @@
 
 extends Node2D
-var SPEED = 200
+export var SPEED = 300
+var dir = 1
 var last_drop_time = 0.0
 var max_drop_time = 2.0
 
@@ -8,15 +9,20 @@ func _ready():
 	set_process(true)
 	
 func _process(delta):
-	var desp = 0
-	if Input.is_action_pressed("ui_left"):
-		desp =  delta * -SPEED
-	if Input.is_action_pressed("ui_right"):
-		desp =  delta * SPEED
-	translate(Vector2(desp,0))
-
-	last_drop_time -= delta
+	# Move the hand
 	var pos = get_pos()
+	var screen = get_viewport().get_rect().size
+
+	if pos.x>=(screen.x-64):
+		dir = -1
+	if pos.x<=(0+64):
+		dir = 1
+		
+	var offset =  delta * SPEED * dir
+	translate(Vector2(offset,0))
+
+	# Drop the block
+	last_drop_time -= delta
 	
 	var drop = Input.is_action_pressed("ui_accept")
 	if (drop and last_drop_time<=0):
